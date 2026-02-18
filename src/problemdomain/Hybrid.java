d3c8baef246014e0f079195c1f44d9b6af091b2f
ryandezall <ryandezall@the-black-pearl.lan>
2026-02-17 21:45:16 -0700

    hybrid class created

---
+++ src/problemdomain/Hybrid.java
@@ -0,0 +1,25 @@
+package problemdomain;
+
+public class Hybrid extends Vehicle {
+	private String powerTrain;
+	
+	public Hybrid(long carID, String vehicleType, String subType, int speed, double fuel, 
+			int seats, int year, String drivetrain, int price, int quantity, String powerTrain) {
+		super(carID, vehicleType, subType, speed, fuel, seats, year, drivetrain, price, quantity);
+		this.powerTrain = powerTrain;
+	}
+
+	public String getPowerTrainType() {
+		switch(powerTrain.toUpperCase()) {
+		case "E": return "Series Hybrid";
+		case "A": return "Parallel Hybrid";
+		case "PHEV": return "Plug-in Hybrid";
+		default: return "Unknown";
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "\n Power Train: " + getPowerTrainType();
+	}
+}
